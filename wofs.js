6bdcf82f43ebacf27e96e6fd83be73a8e3eb8dba
=aminejelass <=aminejelassi95@gmail.com>
2021-10-16 01:05:27 +0200

    wall of shame done with all cards

--- wofs.js
+++ wofs.js
@@ -5,216 +5,252 @@ var liste = [
         lastname: "Karmous",
         imagelink:
             "https://ca.slack-edge.com/T02E5DNSTR6-U02D94AAZ62-1cbe9029bdb9-512",
+        pins: [0, 0, 0, 0],
     },
     {
         name: "ahmed ",
         lastname: "bouafif",
         imagelink:
             "https://ca.slack-edge.com/T02E5DNSTR6-U02E5F792U8-da3cca97550c-512",
+        pins: [0, 0, 0, 0],
     },
     {
         name: "Alia  ",
         lastname: "ouerghuit",
         imagelink:
             "https://ca.slack-edge.com/T02E5DNSTR6-U02E5F7G532-61dfc4e03265-512",
+        pins: [0, 0, 0, 0],
     },
     {
         name: "amine ",
         lastname: "jelassi ",
         imagelink:
             "https://ca.slack-edge.com/T02E5DNSTR6-U02E5F78REU-de544e1bb375-512",
+        pins: [0, 0, 0, 0],
     },
     {
         name: "Amir  ",
         lastname: "Hammami ",
         imagelink:
             "https://ca.slack-edge.com/T02E5DNSTR6-U02E5F7GGJC-a1840b5c4aa2-512",
+        pins: [0, 0, 0, 0],
     },
     {
         name: "aydi  ",
         lastname: "saddem ",
         imagelink:
             "https://ca.slack-edge.com/T02E5DNSTR6-U02E5F75SBA-07578c1aaff8-512",
+        pins: [0, 0, 0, 0],
     },
     {
         name: "aymen ",
         lastname: "nefzi ",
         imagelink:
             "https://ca.slack-edge.com/T02E5DNSTR6-U02E5F764UQ-aebf46029d83-512",
+        pins: [0, 0, 0, 0],
     },
     {
         name: "Aziz  ",
         lastname: "GHATTAS ",
         imagelink:
             "https://ca.slack-edge.com/T02E5DNSTR6-U02E5F7EXK2-21a1422dcfe7-512",
+        pins: [0, 0, 0, 0],
     },
     {
         name: "Badis ",
         lastname: "Naceur ",
         imagelink:
             "https://ca.slack-edge.com/T02E5DNSTR6-U02E5F7EB0Q-23cd3ce6dfa4-512",
+        pins: [0, 0, 0, 0],
     },
     {
         name: "Belguesmi",
         lastname: "Sakr",
         imagelink:
             "https://ca.slack-edge.com/T02E5DNSTR6-U02DFS49BPV-a278752f7db1-512",
+        pins: [0, 0, 0, 0],
     },
     {
         name: "chaabani ",
         lastname: "sarra",
         imagelink:
             "https://ca.slack-edge.com/T02E5DNSTR6-U02E7NWDUJU-8839e434f2be-512",
+        pins: [0, 0, 0, 0],
     },
     {
         name: "Foued ",
         lastname: "charaabi ",
         imagelink:
             "https://ca.slack-edge.com/T02E5DNSTR6-U02E5F79UKS-55dde4db178c-512",
+        pins: [0, 0, 0, 0],
     },
     {
         name: "Ghaoui ",
         lastname: "Bechir ",
         imagelink:
             "https://ca.slack-edge.com/T02E5DNSTR6-U02E5F7CZRN-db76b7591965-512",
+        pins: [0, 0, 0, 0],
     },
     {
         name: "HAMID ",
         lastname: "nooo ",
         imagelink:
             "https://ca.slack-edge.com/T02E5DNSTR6-U02E5F74N48-1f14a7e6d07c-512",
+        pins: [0, 0, 0, 0],
     },
     {
         name: "iheb ",
         lastname: "moujahed ",
         imagelink:
             "https://ca.slack-edge.com/T02E5DNSTR6-U02E5F7FUNL-2fa731f3c28b-512",
+        pins: [0, 0, 0, 0],
     },
     {
         name: "Jesser ",
         lastname: "Barbouchi",
         imagelink:
             "https://ca.slack-edge.com/T02E5DNSTR6-U02E5F73Y3A-e82129deb2f0-512",
+        pins: [0, 0, 0, 0],
     },
     {
         name: "jouini ",
         lastname: "bilel ",
         imagelink:
             "https://ca.slack-edge.com/T02E5DNSTR6-U02E5F7BN00-9d79b99ed1c3-512",
+        pins: [0, 0, 0, 0],
     },
     {
         name: "kais",
         lastname: "najjar ",
         imagelink:
             "https://ca.slack-edge.com/T02E5DNSTR6-U02E5F77ML0-e51cc4f0f79f-512",
+        pins: [0, 0, 0, 0],
     },
     {
         name: "lammouchi",
         lastname: "louay",
         imagelink:
             "https://ca.slack-edge.com/T02E5DNSTR6-U02E5F74Z2L-48e3b8ef1017-512",
+        pins: [0, 0, 0, 0],
     },
     {
         name: "lazhari ",
         lastname: "abdallah",
         imagelink:
             "https://ca.slack-edge.com/T02E5DNSTR6-U02E5F75CEL-6ae6486df41d-512",
+        pins: [0, 0, 0, 0],
     },
     {
         name: "Med ali ",
         lastname: " Baklouti",
         imagelink:
             "https://ca.slack-edge.com/T02E5DNSTR6-U02E5F7DYSU-64b4c87a0552-512",
+        pins: [0, 0, 0, 0],
     },
     {
         name: "Mohamed Amine",
         lastname: "Ben Salah",
         imagelink:
             "https://ca.slack-edge.com/T02E5DNSTR6-U02E5F74BSL-c940af1851d9-512",
+        pins: [0, 0, 0, 0],
     },
     {
         name: "mohamed ",
         lastname: "aymen saadi ",
         imagelink:
             "https://ca.slack-edge.com/T02E5DNSTR6-U02E5F76FUG-c8054f3df35d-512",
+        pins: [0, 0, 0, 0],
     },
     {
         name: "Mohamed ",
         lastname: "Haouach",
         imagelink:
             "https://ca.slack-edge.com/T02E5DNSTR6-U02D3BNHCNT-0439ff3a5a54-512",
+        pins: [0, 0, 0, 0],
     },
     {
         name: "Mortadha ",
         lastname: "abidi ",
         imagelink:
             "https://ca.slack-edge.com/T02E5DNSTR6-U02E5F7CAEL-4613fabd36d5-512",
+        pins: [0, 0, 0, 0],
     },
     {
         name: "Mourad ",
         lastname: "Chniti ",
         imagelink:
             "https://ca.slack-edge.com/T02E5DNSTR6-U02E5F779CG-46c158e96eec-512",
+        pins: [0, 0, 0, 0],
     },
     {
         name: "Naceur ",
         lastname: "mossab ",
         imagelink:
             "https://ca.slack-edge.com/T02E5DNSTR6-U02DFS9F7U2-b6fae1d63120-512",
+        pins: [0, 0, 0, 0],
     },
     {
         name: "Najm ",
         lastname: "abbasi ",
         imagelink:
             "https://ca.slack-edge.com/T02E5DNSTR6-U02E5F7F8JC-00f08d069606-512",
+        pins: [0, 0, 0, 0],
     },
     {
         name: "oussama ",
         lastname: "hmaied",
         imagelink:
             "https://ca.slack-edge.com/T02E5DNSTR6-U02E5F7DA64-b68d6835c8c5-512",
+        pins: [0, 0, 0, 0],
     },
     {
         name: "Sahar  ",
         lastname: "noo ",
         imagelink:
             "https://ca.slack-edge.com/T02E5DNSTR6-U02E5F7A96U-3007546eb85d-512",
+        pins: [0, 0, 0, 0],
     },
     {
         name: "SARAH ",
         lastname: "BEN ALI ",
         imagelink:
             "https://ca.slack-edge.com/T02E5DNSTR6-U02E5F7GXGQ-f761f0901f57-512",
+        pins: [0, 0, 0, 0],
     },
     {
         name: "Arbi fares ",
         lastname: "Sghaier",
         imagelink:
             "https://ca.slack-edge.com/T02E5DNSTR6-U02E5F7B9QQ-bf890db01ccc-512",
+        pins: [0, 0, 0, 0],
     },
     {
         name: "shayma  ",
         lastname: "Naffeti",
         imagelink:
             "https://ca.slack-edge.com/T02E5DNSTR6-U02E5F78D6U-dcec32b49f0a-512",
+        pins: [0, 0, 0, 0],
     },
     {
         name: "Ghassen  ",
         lastname: "Tababi",
         imagelink:
             "https://ca.slack-edge.com/T02E5DNSTR6-U02D3CEBNR5-578b95b59208-512",
+        pins: [0, 0, 0, 0],
     },
     {
         name: "Wael  ",
         lastname: " Ajabi ",
         imagelink:
             "https://ca.slack-edge.com/T02E5DNSTR6-U02E5F7BZL0-537f4e38e96b-512",
+        pins: [0, 0, 0, 0],
     },
     {
         name: "Wassim  ",
         lastname: "el fen ",
         imagelink:
             "https://ca.slack-edge.com/T02E5DNSTR6-U02E5F79FG8-67d5c3ca178d-512",
+        pins: [0, 0, 0, 0],
     },
 ];
 
@@ -228,7 +264,7 @@ $( document ).ready(function ()
     
 
      
-       $("#container").append(`<div class="card">
+          $("#container").append(`<div class="card">
             <div class="image">
                 <img
                     src="`+liste[i].imagelink+`"
@@ -239,8 +275,14 @@ $( document ).ready(function ()
             </div>
             <div class="des">
                 <p>You can Add Desccription Here...</p>
-                <button>Read More...</button>
+
+            
             </div>
+                <div id="red" onclick="redpin(`+i+`)" >`+liste[i].pins[0]+`</div>
+                <div id="blue" onclick="bluepin(`+i+`)">`+liste[i].pins[1]+`</div>
+                <div id="yellow" onclick="yellowpin(`+i+`)">`+liste[i].pins[2]+`</div>
+                <div id="green" onclick="greenpin(`+i+`)">`+liste[i].pins[3]+`</div>
+
         </div>`);       
    }
 
@@ -294,8 +336,7 @@ function rend(lst)
    {   
     
 
-     
-       $("#container").append(`<div class="card">
+          $("#container").append(`<div class="card">
             <div class="image">
                 <img
                     src="`+lst[i].imagelink+`"
@@ -306,9 +347,15 @@ function rend(lst)
             </div>
             <div class="des">
                 <p>You can Add Desccription Here...</p>
-                <button>Read More...</button>
+
+            
             </div>
-        </div>`);       
+                <div id="red" onclick="redpin(`+i+`)" >`+lst[i].pins[0]+`</div>
+                <div id="blue" onclick="bluepin(`+i+`)">`+lst[i].pins[1]+`</div>
+                <div id="yellow" onclick="yellowpin(`+i+`)">`+lst[i].pins[2]+`</div>
+                <div id="green" onclick="greenpin(`+i+`)">`+lst[i].pins[3]+`</div>
+
+        </div>`);      
    }
 
 }
@@ -326,8 +373,7 @@ function refresh()
    {   
     
 
-     
-       $("#container").append(`<div class="card">
+          $("#container").append(`<div class="card">
             <div class="image">
                 <img
                     src="`+liste[i].imagelink+`"
@@ -338,11 +384,70 @@ function refresh()
             </div>
             <div class="des">
                 <p>You can Add Desccription Here...</p>
-                <button>Read More...</button>
+
+            
             </div>
-        </div>`);       
+                <div id="red" onclick="redpin(`+i+`)" >`+liste[i].pins[0]+`</div>
+                <div id="blue" onclick="bluepin(`+i+`)">`+liste[i].pins[1]+`</div>
+                <div id="yellow" onclick="yellowpin(`+i+`)">`+liste[i].pins[2]+`</div>
+                <div id="green" onclick="greenpin(`+i+`)">`+liste[i].pins[3]+`</div>
+
+        </div>`); 
+     
+        
    
 
 }
 
 }
+
+
+function redpin(i)
+{
+
+   liste[i].pins[0]+=1
+   refresh()
+
+}
+function bluepin(i)
+{
+   if(liste[i].pins[1]>=5)
+   {
+    liste[i].pins[0]+=1
+    liste[i].pins[1]=0;
+    refresh()
+
+   } 
+   else{
+   liste[i].pins[1]+=1
+   refresh()}
+
+}
+function yellowpin(i)
+{
+ if(liste[i].pins[2]>=5)
+   {
+    liste[i].pins[0]+=1
+    liste[i].pins[2]=0;
+    refresh()
+
+   } 
+ else
+ {  liste[i].pins[2]+=1
+   refresh()}
+
+}
+function greenpin(i)
+{
+    if(liste[i].pins[3]>=5)
+   {
+    liste[i].pins[0]+=1
+    liste[i].pins[3]=0;
+    refresh()
+
+   } 
+  else{liste[i].pins[3]+=1
+   refresh()}
+
+}
+
